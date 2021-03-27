46611f8030f0d90d6bbfa191f5e3257cce80c3c4
nxtfun <machina.posteri@gmail.com>
2021-03-27 23:20:08 +0100

    Added countdown. Bugfixes.

--- sketch.js
+++ sketch.js
@@ -19,31 +19,36 @@ let databaseLocation = 'https://raw.githubusercontent.com/nxtfun/VocalHero/main/
 let jsonContainer//json container to load data to
 let json = {}; //new JSON Object to save data to
 let radio //radio buttons
-let colour 
+let colour
 let controllers = []
 let connected = 0;
 let sensor = 0;
-let previousMillis = 0;
+let previousMillis1 = 0;
 let previousMillis2 = 0;
+let previousMillis3 = 0;
 let timestamp = 0;
+let timestamp3 = 0;
+let timer1 = 0;
 let currentMillis = 0;
 let secinsample = 10; //how many ms between samples
-let przesuw=0;
+let przesuw = 0;
 let xvals = []; // values from sensor saved to buffer
 let xtime = []; // time stamps for every sensor value
 let xvals2 = []; // values from file saved to buffer
 let xtime2 = []; // time stamps for every sensor value from file
+let exercise2 = []; // exercise from json
 
 let startButton;
 let stopButton;
 let radioVal; //radio button value
 
-let di=0;
+let di = 0;
 let pixpsec = 0.10;//how many pixels in 1 ms on chart
 let rduration = 10000;//how long is one recording
-let chartlen = pixpsec*rduration;//length of chart in px
+let chartlen = pixpsec * rduration;//length of chart in px
 
-let startState = 0;//state of chart  '1' after start, '0 when stopped'
+let startState = 0;//state of "Start" button  0 - unlocked, 1 - locked (after start, to not push it again)
+let startState2 = 0;//state of chart  '1' after countdown, '0 when stopped'
 
 //id and markers data
 let taskType = 1; //which module, from (1-3)
@@ -58,7 +63,12 @@ let days;
 
 let mic, recorder, soundFile; //audio recorder
 
+let startCountdown = 0;
 
+let fade1 = 255;
+let fade2 = 255;
+let fade3 = 255;
+let fade4 = 255;
 
 //loadSound('https://raw.githubusercontent.com/nxtfun/VocalHero/main/database/1_mod1_24-3-2021_23-26-6_kkk.wav')
 
@@ -72,56 +82,57 @@ let mic, recorder, soundFile; //audio recorder
 //x= stamp*pixpsec
 
 function preload() {
-  
+
   //jsonContainer = loadJSON('ciastko.json');
   jsonContainer = loadJSON(databaseLocation + '2/1.json');
   sound = loadSound(databaseLocation + '2/1.wav');
-  
+
 }
 
 function setup() {
-//start of setup
-  
+  //start of setup
+
   //input text
-  input = createInput();
-  input.position(400, 5);
-  
-  
-  
+  //input = createInput();
+  //input.position(400, 5);
+
+
+
   //radio buttons
-  radio = createRadio(); 
-  radio.option('1');
-  radio.option('2');
-  radio.option('3');
-  radio.style('width', '200px');
-  radio.style('height', '20px');
-  radio.position(300,5);
-  
-  
+  //radio = createRadio();
+  //radio.option('1');
+  //radio.option('2');
+  //radio.option('3');
+  //radio.style('width', '200px');
+  //radio.style('height', '20px');
+  //radio.position(300, 5);
+
+
   //buttons
   startButton = createButton('START!');
-  startButton.position(560,5);
+  startButton.position(560, 5);
   startButton.mousePressed(startButtonF);
-  
+
   //stopButton = createButton('STOP!');
   //stopButton.position(50,90);
   //stopButton.mousePressed(stopButtonF);
-  
+
   textFont("Montserrat");
   xvals2 = Object.values(jsonContainer.values);
   xtime2 = Object.values(jsonContainer.timestamp);
-  
-//console.log(xvals2);
-//xvals2 = Object.values(xvals2);//to check if valid
-//console.log(typeof xvals2);
-console.log(xtime2[21]);
- 
-  
-  
-  
-  
+  exercise2 = jsonContainer.exercise;
+
+  //console.log(xvals2);
+  //xvals2 = Object.values(xvals2);//to check if valid
+  //console.log(typeof xvals2);
+  console.log(xtime2[21]);
+
+
+
+
+
   //audio recorder
-    // create an audio in
+  // create an audio in
   mic = new p5.AudioIn();
 
   // users must manually enable their browser microphone for recording to work properly!
@@ -135,37 +146,37 @@ console.log(xtime2[21]);
 
   // create an empty sound file that we will use to playback the recording
   soundFile = new p5.SoundFile();
-  
-  
 
-  
-  
-  
+
+
+
+
+
   //colour=color('#white')
-  
-  
-  
+
+
+
   createCanvas(1100, 400)
   //background(120,200,0)
   noStroke()
-  window.addEventListener("gamepadconnected", function(e) {
-  gamepadHandler(e, true);
-  //console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
+  window.addEventListener("gamepadconnected", function (e) {
+    gamepadHandler(e, true);
+    //console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
     //e.gamepad.index, e.gamepad.id,
     //e.gamepad.buttons.length, e.gamepad.axes.length);
     connected = 1;
   });
-	window.addEventListener("gamepaddisconnected", function(e) {
-  //console.log("Gamepad disconnected from index %d: %s",
-  //  e.gamepad.index, e.gamepad.id);      
+  window.addEventListener("gamepaddisconnected", function (e) {
+    //console.log("Gamepad disconnected from index %d: %s",
+    //  e.gamepad.index, e.gamepad.id);      
     //colour=color(120,120,0)
     gamepadHandler(e, false);
     connected = 0;
-  }); 
-  
-  
+  });
+
+
   //frameRate(10);
-  
+
 }
 //end of setup
 
@@ -186,225 +197,340 @@ console.log(xtime2[21]);
 //mic.enabled
 
 function draw() {
-//clear();  
-background ('#fb8500');
-  
-  exerciseData=input.value();
-  
-  
-  
-  
-  
-  
-  
-  
+  //clear();  
+  background('#fb8500');
+
+  //exerciseData = input.value();//changes char input to int input;
+  exerciseData = exercise2;
+
+
+
+
+
+
+
   //radioVal = radio.value();
-  
- 
-  
-//background (255,183,3)
 
-//print(xvals2[])  ;
-  
-//Input from sensor
-if(!connected){
-  if((-mouseY+332)>=0 &&(-mouseY+332)<131){
-    sensor = -mouseY+332;
-  }
-  else if((-mouseY+332)>=131){
-    sensor = 130;
-          
-          }
-  else{
-    
-    
-    sensor = 0;
-  }
-  
-}
-  else{
-//sensor = round(axisInput(),2); 
-  sensor = axisInput();
-  //sensor = round(sensor,2);  
 
-  } 
-  
-  
-  push()  
-strokeWeight(10);
+
+  //background (255,183,3)
+
+  //print(xvals2[])  ;
+
+  //Input from sensor
+  if (!connected) {
+    if ((-mouseY + 332) >= 0 && (-mouseY + 332) < 131) {
+      sensor = -mouseY + 332;
+    }
+    else if ((-mouseY + 332) >= 131) {
+      sensor = 130;
+
+    }
+    else {
+
+
+      sensor = 0;
+    }
+
+  }
+  else {
+    //sensor = round(axisInput(),2); 
+    sensor = axisInput();
+    //sensor = round(sensor,2);  
+
+  }
+
+
+  push()
+  strokeWeight(10);
   //fill(200)
-fill(204, 153, 0);
+  fill(204, 153, 0);
   fill('#FFB703')
-rect(0,height,width,-sensor*2+30);
-pop()
-  
-  
-  
-    push()  
-strokeWeight(10);
+  rect(0, height, width, -sensor * 2 + 30);
+  pop()
+
+
+
+  push()
+  strokeWeight(10);
   //fill(200)
-fill(250);
+  fill(250);
   //fill('#FFB703')
-rect(50,20,135,70,10);
-pop()
-  
-  
-  
-    text('ID:',700,20);
-  text(taskIndex,720,20);//which index is now playing
-  
+  rect(50, 20, 135, 70, 10);
+  pop()
+
+
+
+  //text('ID:', 700, 20);
+  //text(taskIndex, 720, 20);//which index is now playing
+
   push();
   fill(250)
   strokeWeight(0);
   stroke(150);
-  rect(50, 105, chartlen+1, 250,10);
+  rect(50, 105, chartlen + 1, 250, 10);
   pop();
+
+  //time  
+  currentMillis = millis();
+
+
+
+  if (startCountdown) {
+
+
+
+    timestamp3 = currentMillis - previousMillis3;
+    if (timestamp3 > 0 && timestamp3 < 1000) {
+
+      push()
+      fill(0, 0, 0, fade1)
+      textSize(60);
+      textAlign(CENTER);
+      text('3', width / 2, 250);
+      pop()
+
+      fade1 = fade1 - 5;
+    }
+
+    if (timestamp3 > 1000 && timestamp3 < 2000) {
+
+      push()
+      fill(0, 0, 0, fade2)
+      textSize(60);
+      textAlign(CENTER);
+      text('2', width / 2, 250);
+      pop()
+
+      fade2 = fade2 - 5;
+
+    }
+
+    if (timestamp3 > 2000 && timestamp3 < 3000) {
+
+      push()
+      fill(0, 0, 0, fade3)
+      textSize(60);
+      textAlign(CENTER);
+      text('1', width / 2, 250);
+      pop()
+
+      fade3 = fade3 - 5;
+    }
+
+
+    if (timestamp3 > 3000 && timestamp3 < 3500) {
+
+      push()
+      fill(0, 0, 0, fade4)
+      textSize(60);
+      textAlign(CENTER);
+      text('START!', width / 2, 250);
+      pop()
+
+      fade4 = fade4 - 10;
+
+    }
+
+
+    if (timestamp3 > 3000) {
+
+      startState2 = 1; //start chart
+
+    }
+
+
+    if (timestamp3 > 4000) {
+
+      startCountdown = 0; //stop this function
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+
+
+
+  /*
   
-//time  
-currentMillis = millis();
-//  if(currentMillis - previousMillis > secinsample){
-//    previousMillis = currentMillis;
-    //print('Hello');
-//    if(przesuw>300)przesuw = 0;
-//    przesuw = przesuw+1;
-//  }
+      previousMillis1 = currentMillis; //reset timer
+      timer1 = 0; //reset timer
   
+      while (timer1 < 3000) {
+  
+  
+        timer1 = millis() - previousMillis1;//current time from start
+  
+        if (1) {
+  
+  
+          push()
+          textSize(40);
+          textAlign(CENTER);
+          text('3', width / 2, 200);
+          pop()
+  
+        }
+  
+      }
+  */
+
+
+
+  //  if(currentMillis - previousMillis > secinsample){
+  //    previousMillis = currentMillis;
+  //print('Hello');
+  //    if(przesuw>300)przesuw = 0;
+  //    przesuw = przesuw+1;
+  //  }
+
 
   //save input data to buffer
-  if(startState){
-  startRecording();
+  if (startState2) {
+    startRecording();
   }
   else {
     previousMillis2 = currentMillis;
     //xvals = [];//erase data
-    di=0;
+    di = 0;
   }
-  
-    
- // push()  
-//strokeWeight(10);
+
+
+  // push()  
+  //strokeWeight(10);
   //fill(200)
-//fill(204, 153, 0);
+  //fill(204, 153, 0);
   //fill('#FFB703')
-//ellipse(width/2 -20, height/2 +55,180,180);
-//pop()
-  
-  
-  
-  
-  
-//push()  
-//strokeWeight(10);
-//  fill(200)
-//fill(204, 153, 0);
-//  fill('#FFB703')
-//ellipse(750, 0,50+ sensor*2,50+ sensor*2);
-//pop()
-  
-    push()
+  //ellipse(width/2 -20, height/2 +55,180,180);
+  //pop()
+
+
+
+
+
+  //push()  
+  //strokeWeight(10);
+  //  fill(200)
+  //fill(204, 153, 0);
+  //  fill('#FFB703')
+  //ellipse(750, 0,50+ sensor*2,50+ sensor*2);
+  //pop()
+
+  push()
   strokeWeight(2);
   stroke('#0CCA4A');
-  line(52, 430-sensor*2, chartlen+48+1, 430-sensor*2);
- 
-  
+  line(52, 430 - sensor * 2, chartlen + 48 + 1, 430 - sensor * 2);
+
+
   pop()
-  push()  
-strokeWeight(2);
+  push()
+  strokeWeight(2);
   stroke(200);
-  
-  
-  
-  
-line(52,430-linia1*2, chartlen+48+1,430-linia1*2);
-  
-  
-line(52,430-linia2*2, chartlen+48+1,430-linia2*2);
-  
-  
-line(52,430-linia3*2, chartlen+48+1,430-linia3*2);
-  
-line(52,430-linia4*2, chartlen+48+1,430-linia4*2);
-  
-line(52,430-linia5*2, chartlen+48+1,430-linia5*2);
-  
-line(52,430-linia6*2, chartlen+48+1,430-linia6*2);
-  
-line(52,430-linia7*2, chartlen+48+1,430-linia7*2);
-  
-line(52,430-linia8*2, chartlen+48+1,430-linia8*2);
-  
-  
-  
+
+
+
+
+  line(52, 430 - linia1 * 2, chartlen + 48 + 1, 430 - linia1 * 2);
+
+
+  line(52, 430 - linia2 * 2, chartlen + 48 + 1, 430 - linia2 * 2);
+
+
+  line(52, 430 - linia3 * 2, chartlen + 48 + 1, 430 - linia3 * 2);
+
+  line(52, 430 - linia4 * 2, chartlen + 48 + 1, 430 - linia4 * 2);
+
+  line(52, 430 - linia5 * 2, chartlen + 48 + 1, 430 - linia5 * 2);
+
+  line(52, 430 - linia6 * 2, chartlen + 48 + 1, 430 - linia6 * 2);
+
+  line(52, 430 - linia7 * 2, chartlen + 48 + 1, 430 - linia7 * 2);
+
+  line(52, 430 - linia8 * 2, chartlen + 48 + 1, 430 - linia8 * 2);
+
+
+
   //line(50+przesuw,330,50+przesuw,200)
   pop()
-  
+
   //draw chart
-  push() 
+  push()
   stroke(100);//line color
   strokeWeight(2.6);//line weight
-  
-    
-  for (let i = 0; i < xvals.length; i++) {
-    line(pixpsec*xtime[i]+50,-xvals[i]*2+430,pixpsec*xtime[i+1]+50,-xvals[i+1]*2+430);
-    
-  }
-  
-  stroke(100,200,300);
 
-  
+
+  for (let i = 0; i < xvals.length; i++) {
+    line(pixpsec * xtime[i] + 50, -xvals[i] * 2 + 430, pixpsec * xtime[i + 1] + 50, -xvals[i + 1] * 2 + 430);
+
+  }
+
+  stroke(100, 200, 300);
+
+
   for (let i = 0; i < xvals2.length; i++) {
-    line(pixpsec*xtime2[i]+50,-xvals2[i]+330,pixpsec*xtime2[i+1]+50,-xvals2[i+1]+330);
-    
+    line(pixpsec * xtime2[i] + 50, -xvals2[i] + 330, pixpsec * xtime2[i + 1] + 50, -xvals2[i + 1] + 330);
+
   }
   stroke(100);
 
-  
-    
-  
 
 
-  
+
+
+
+
   //line(50, 335, 30+(chartlen), 335);
-pop()
-  
+  pop()
 
-  
-  
 
-push();
-textSize(40)
-text((round(sensor*100))/100, 60, 70);
-pop(); 
-  
 
-text('Podłączony: ',800,20);
-//text('Millis: ',10,60);
-//text(round(millis()),50,60);
- 
-  
-if(connected){
-  text('TAK!',870,20);
-}
-  else {
-    text('NIE',870,20);
+  push();
+  textSize(40)
+  text((round(sensor * 100)) / 100, 60, 70);
+  pop();
+
+
+  text('Podłączony: ', 800, 20);
+  //text('Millis: ',10,60);
+  //text(round(millis()),50,60);
+
+
+  if (connected) {
+    text('TAK!', 870, 20);
   }
-  
+  else {
+    text('NIE', 870, 20);
+  }
 
 
-push()
-textSize(30);
-//text('Lubisz ciastka?',250,140);
-  text(exerciseData,200,140);
-  
-  
-pop()  
 
-  
-  
-  
-  
-  
-  
+  push()
+  textSize(30);
+  textAlign(CENTER);
+  text(exerciseData, 0, 140, width);
+
+
+  pop()
+
+
+
+
+
+
+
+
+
+
 }
 
 
@@ -412,7 +538,7 @@ pop()
 function gamepadHandler(event, connecting) {
   let gamepad = event.gamepad;
   if (connecting) {
- 		 //print("Connecting to controller "+gamepad.index)
+    //print("Connecting to controller "+gamepad.index)
     controllers[gamepad.index] = gamepad
   } else {
     delete controllers[gamepad.index]
@@ -423,114 +549,125 @@ function gamepadHandler(event, connecting) {
 
 
 
-function axisInput()
-{ 
-  if(connected){
-  var gamepads = navigator.getGamepads()
-  for (let i in controllers)
-  {
-    let controller=gamepads[i]//controllers[i]
-    
-    //return 100+controller.axes[7]*100;
-    return 100+(controller.axes[10]+controller.axes[7])*100;
-   
+function axisInput() {
+  if (connected) {
+    var gamepads = navigator.getGamepads()
+    for (let i in controllers) {
+      let controller = gamepads[i]//controllers[i]
+
+      //return 100+controller.axes[7]*100;
+      return 100 + (controller.axes[10] + controller.axes[7]) * 100;
+
+    }
   }
-  }
-  else{
+  else {
     return 0;
   }
 }
 
 
 function startButtonF() {
-  if(!startState && radio.value()){
-  getAudioContext().resume();
-  sound.play();
-  console.log('Start!');
-  taskIndex++;
-  xvals = [];//erase data
-  startState = 1;
-  di=0;
-    
-  recorder.record(soundFile);//record audio  
-    
-    
-    }
+  if (!startState) {
+    startState = 1;
+    fade1 = 255;
+    fade2 = 255;
+    fade3 = 255;
+    fade4 = 255;
+    previousMillis3 = currentMillis;
+    startCountdown = 1;
+
+    xvals = [];//erase data
+    getAudioContext().resume(); //needed by browser to use microphone and audio
+
+
+    sound.play();
+    console.log('Start!');
+    taskIndex++;
+
+
+    di = 0;//xvals[di] = sensor
+
+    recorder.record(soundFile);//record audio  
+
+
+  }
 }
 
 function stopButtonF() {
   console.log('Stop!');
   startState = 0;
+  startState2 = 0;
   previousMillis2 = currentMillis;
-  
+
 }
 
 
 function startRecording() {
-  
-  timestamp = round(currentMillis - previousMillis2);//current time in ms from start of recording data
-    
-    
-    
-    
-  //If data gathering time is longer than (value of time in ms), reset the timestamp.
-    if(timestamp>rduration){
-      //di=0;
-      //previousMillis2 = currentMillis;
-      
-      recorder.stop();//stop recording audio
-      
-      
-      
-      
-      hours = hour();
-      minutes = minute();
-      seconds = second();
-      years = year();
-      months = month();
-      days = day();
-      
 
-      
-      
-      json.id = taskIndex;
-      json.module = radio.value();
-      json.exercise = exerciseData;
-      json.date = (str(days) + '.' + str(months) + '.' + str(years));
-      json.hour = (str(hours) + ':' + str(minutes) + ':' + str(seconds));
-      
-      
-      
-      json.values = xvals;
-      json.timestamp = xtime;
-      
-      if(zapis){
-      saveJSON(json,str(taskIndex) + '_' + 'mod' + str(radio.value() + '_' + str(days) + '-' + str(months) + '-' + str(years) + '_' + str(hours) + '-' + str(minutes) + '-' + str(seconds) + '_' + exerciseData));
-      
-      
-      saveSound(soundFile,str(taskIndex) + '_' + 'mod' + str(radio.value() + '_' + str(days) + '-' + str(months) + '-' + str(years) + '_' + str(hours) + '-' + str(minutes) + '-' + str(seconds) + '_' + exerciseData)); // save file
-      }
-      
-      stopButtonF();
-      
-      //xvals = [];//erase data
-      //print(xtime);
+  timestamp = round(currentMillis - previousMillis2);//current time in ms from start of recording data
+
+
+
+
+  //If data gathering time is longer than (value of time in ms), reset the timestamp.
+  if (timestamp > rduration) {
+    //di=0;
+    //previousMillis2 = currentMillis;
+
+    recorder.stop();//stop recording audio
+
+
+
+
+    hours = hour();
+    minutes = minute();
+    seconds = second();
+    years = year();
+    months = month();
+    days = day();
+
+
+
+
+    json.id = taskIndex;
+    //json.module = radio.value();
+    json.exercise = exerciseData;
+    json.date = (str(days) + '.' + str(months) + '.' + str(years));
+    json.hour = (str(hours) + ':' + str(minutes) + ':' + str(seconds));
+
+
+
+    json.values = xvals;
+    json.timestamp = xtime;
+
+
+    if (zapis) {
+      saveJSON(json, str(taskIndex) + '_' + 'mod' + '_' + str(days) + '-' + str(months) + '-' + str(years) + '_' + str(hours) + '-' + str(minutes) + '-' + str(seconds) + '_' + exerciseData);
+
+
+      saveSound(soundFile, str(taskIndex) + '_' + 'mod' + '_' + str(days) + '-' + str(months) + '-' + str(years) + '_' + str(hours) + '-' + str(minutes) + '-' + str(seconds) + '_' + exerciseData); // save file
     }
-  else{
-    
+
+    stopButtonF();
+
+    //xvals = [];//erase data
+    //print(xtime);
+  }
+  else {
+
     xvals[di] = sensor; //save sensor data with every frame
     xtime[di] = timestamp; //save time from start in ms with every frame
     di++;//iterate to next frame
   }
-  
-  text('time:',10,130);
-  text(timestamp,40,130);
-  text('frame:',10,145)
-  text(di,45,145)
-  
+
+  text('time:', 10, 130);
+  text(timestamp, 40, 130);
+  text('frame:', 10, 145)
+  text(di, 45, 145)
+
 }
 
-function saveCSV(){
-  
+function saveCSV() {
+
 }
 
